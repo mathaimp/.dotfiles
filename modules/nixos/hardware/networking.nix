b27d2fee35f0b197e41f30c359ba10c833a1b47d
Mathai Mathew <mathewp8616@gmail.com>
2026-09-24 01:16:36 +0530

    Use 1.1.1.1 as default dns

--- modules/nixos/hardware/networking.nix
+++ modules/nixos/hardware/networking.nix
@@ -9,9 +9,13 @@
       settings.Resolve = {
         DNSSEC = "true";
         Domains = [ "~." ];
+        DNS = [
+          "1.1.1.1"
+          "[2606:4700:4700::1111]"
+        ];
         FallbackDNS = [
-          "1.1.1.1#one.one.one.one"
-          "1.0.0.1#one.one.one.one"
+          "1.0.0.1"
+          "[2606:4700:4700::1001]"
         ];
         DNSOverTLS = "opportunistic";
       };
